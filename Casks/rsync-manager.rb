753f003b217081b4421b21fd0635b819cc67c361
CyberBison <gaspard@rosay.io>
2025-01-25 17:44:13 +0100

    Updating installer version

--- Casks/rsync-manager.rb
+++ Casks/rsync-manager.rb
@@ -1,6 +1,6 @@
 cask "rsync-manager" do
     version "1.0.0"
-    sha256 "e31e698871bb6a110ad7047f7bbdf7362720841121fdfbafed5ccf09c50e60ef"
+    sha256 "39f76f6d21b335c42ceff16db0ca6e5e86f4b726899a433bcea1985d15dcdd59"
   
     url "https://github.com/CyberBison/rsync-manager/releases/download/v-1.0.0/rsync-manager.dmg"
     name "Rsync Manager"
