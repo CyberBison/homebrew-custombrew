81b7091647961ae6259b1acee5086b6edda200fc
CyberBison <gaspard@rosay.io>
2025-01-25 13:00:32 +0100

    New shasum

--- Casks/rsync-manager.rb
+++ Casks/rsync-manager.rb
@@ -1,6 +1,6 @@
 cask "rsync-manager" do
     version "1.0.0"
-    sha256 "58a233cec5ca57b3d9ef28902df4fcb9f9f882682bcb95513a48804b0fa238e6"
+    sha256 "e2e809b3b050a79955a40e4f5bfd419655fc5951ec2898060e981a737ba40517"
   
     url "https://github.com/CyberBison/rsync-manager/releases/download/v-1.0.0/rsync-manager.zip"
     name "Rsync Manager"
