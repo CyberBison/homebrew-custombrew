5604f807b8ae554662271928e8f1b5071ade0ad0
CyberBison <gaspard@rosay.io>
2025-01-26 22:02:17 +0100

    Now hosting on personal website

--- Casks/rsync-manager.rb
+++ Casks/rsync-manager.rb
@@ -2,10 +2,10 @@ cask "rsync-manager" do
     version "1.0.1"
     sha256 "b170bb83f922bc198bbe35b9674a8f7eb6c3004e7d4238a3762ec8e996f740c3"
   
-    url "https://github.com/CyberBison/rsync-manager/releases/download/v-1.0.1/rsync-manager-1.0.1.dmg"
+    url "https://rsync.cyberbison.io/rsync-manager.dmg"
     name "Rsync Manager"
     desc "A macOS app to manage rsync backups easily"
-    homepage "https://github.com/CyberBison/rsync-manager"
+    homepage "https://rsync.cyberbison.io"
   
     app "rsync-manager.app"
   
