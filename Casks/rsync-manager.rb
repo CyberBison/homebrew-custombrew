435e79e4e23d2159f7e78ebd038885134ea20d7a
CyberBison <gaspard@rosay.io>
2025-02-19 11:39:38 +0100

    changing url 2

--- Casks/rsync-manager.rb
+++ Casks/rsync-manager.rb
@@ -2,7 +2,7 @@ cask "rsync-manager" do
     version "1.0.2"
     sha256 "8e78975bba90135da6e65a73f07f20b81fe8c0085d990f344fd956e9467a869b"
   
-    url "https://rsync.cyberbison.io/rsync-manager.dmg"
+    url "https://rsync.cyberbison.dev/rsync-manager.dmg"
     name "Rsync Manager"
     desc "A macOS app to manage rsync backups easily"
     homepage "https://rsync.cyberbison.dev"
